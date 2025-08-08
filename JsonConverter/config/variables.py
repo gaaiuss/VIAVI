6c7960129b7132c26d9d6581873429f8a32c9519
gaaiuss <caio.gui.castro@gmail.com>
2025-08-08 17:35:49 -0300

    gui vars

--- JsonConverter/config/variables.py
+++ JsonConverter/config/variables.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 # Directories
@@ -6,10 +5,19 @@ ROOT_DIR = Path(__file__).parent.parent
 RESOURCE_DIR = ROOT_DIR / "resource"
 RESULT_DIR = ROOT_DIR / "output"
 CONFIG_DIR = Path(__file__).parent
-
-os.mkdir(RESOURCE_DIR)
+IMG_DIR = ROOT_DIR / "img"
 
 # Files
 JSON_OUTPUT_FILE = CONFIG_DIR / "merged.json"
 CSV_CONFIG_FILE = CONFIG_DIR / "config.csv"
 CSV_OUTPUT_FILE = RESULT_DIR / "output.csv"
+
+# GUI
+ICON_PATH = IMG_DIR / "icon.png"
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 12
+MINIMUM_WIDTH = 1000
+TEXT_MARGIN = 10
