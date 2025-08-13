bffd30d6cdf3b7b71b95b4f9e6d434b33ea85e49
gaaiuss <caio.gui.castro@gmail.com>
2025-08-13 15:13:33 -0300

    UI first version

--- JsonConverter/src/windowUI.py
+++ JsonConverter/src/windowUI.py
@@ -1,81 +1,102 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-# Form generated from reading UI file 'window.ui'
+## Form generated from reading UI file 'window.ui'
 ##
-# Created by: Qt User Interface Compiler version 6.9.1
+## Created by: Qt User Interface Compiler version 6.9.1
 ##
-# WARNING! All changes made in this file will be lost when recompiling UI file!
+## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-                            QMetaObject, QObject, QPoint, QRect, QSize, Qt,
-                            QTime, QUrl)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-                           QFontDatabase, QGradient, QIcon, QImage,
-                           QKeySequence, QLinearGradient, QPainter, QPalette,
-                           QPixmap, QRadialGradient, QTransform)
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
-                               QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-                               QStatusBar, QWidget)
-
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(912, 703)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.gridLayout = QGridLayout()
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.gridLayout.setContentsMargins(0, -1, -1, -1)
-        self.labelStandard = QLabel(self.centralwidget)
-        self.labelStandard.setObjectName(u"labelStandard")
-        self.labelStandard.setMaximumSize(QSize(16777215, 100))
+        self.mainGrid = QGridLayout()
+        self.mainGrid.setObjectName(u"mainGrid")
+        self.mainGrid.setContentsMargins(0, -1, -1, -1)
+        self.buttonsGrid = QGridLayout()
+        self.buttonsGrid.setObjectName(u"buttonsGrid")
+        self.buttonsGrid.setHorizontalSpacing(50)
+        self.buttonsGrid.setContentsMargins(100, -1, -1, -1)
+        self.convertButton = QPushButton(self.centralwidget)
+        self.convertButton.setObjectName(u"convertButton")
+        self.convertButton.setMinimumSize(QSize(0, 100))
+        self.convertButton.setMaximumSize(QSize(200, 16777215))
+
+        self.buttonsGrid.addWidget(self.convertButton, 1, 0, 1, 1)
+
+        self.selectButton = QPushButton(self.centralwidget)
+        self.selectButton.setObjectName(u"selectButton")
+        self.selectButton.setMinimumSize(QSize(0, 100))
+        self.selectButton.setMaximumSize(QSize(200, 16777215))
+
+        self.buttonsGrid.addWidget(self.selectButton, 0, 0, 1, 1)
+
+        self.selectedFilesLabel = QLabel(self.centralwidget)
+        self.selectedFilesLabel.setObjectName(u"selectedFilesLabel")
+
+        self.buttonsGrid.addWidget(self.selectedFilesLabel, 0, 1, 1, 1)
+
+        self.savedReportPath = QLabel(self.centralwidget)
+        self.savedReportPath.setObjectName(u"savedReportPath")
+
+        self.buttonsGrid.addWidget(self.savedReportPath, 1, 1, 1, 1)
+
+
+        self.mainGrid.addLayout(self.buttonsGrid, 2, 0, 1, 1)
+
+        self.labelsLayout = QHBoxLayout()
+        self.labelsLayout.setSpacing(50)
+        self.labelsLayout.setObjectName(u"labelsLayout")
+        self.labelsLayout.setContentsMargins(20, -1, -1, -1)
+        self.labelLogo = QLabel(self.centralwidget)
+        self.labelLogo.setObjectName(u"labelLogo")
+        self.labelLogo.setMaximumSize(QSize(400, 70))
+        self.labelLogo.setPixmap(QPixmap(u"../img/ui_icon.png"))
+        self.labelLogo.setScaledContents(True)
+        self.labelLogo.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.labelLogo.setMargin(0)
+
+        self.labelsLayout.addWidget(self.labelLogo)
+
+        self.labelText = QLabel(self.centralwidget)
+        self.labelText.setObjectName(u"labelText")
+        self.labelText.setMaximumSize(QSize(16777215, 100))
         font = QFont()
+        font.setFamilies([u"Arial"])
         font.setPointSize(20)
-        self.labelStandard.setFont(font)
-        self.labelStandard.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.labelText.setFont(font)
+        self.labelText.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.gridLayout.addWidget(self.labelStandard, 0, 0, 1, 1)
+        self.labelsLayout.addWidget(self.labelText)
 
-        self.gridLayout_2 = QGridLayout()
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setHorizontalSpacing(150)
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setMinimumSize(QSize(0, 100))
-        self.pushButton_2.setMaximumSize(QSize(200, 16777215))
 
-        self.gridLayout_2.addWidget(self.pushButton_2, 1, 0, 1, 1)
+        self.mainGrid.addLayout(self.labelsLayout, 0, 0, 1, 1)
 
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setMinimumSize(QSize(0, 100))
-        self.pushButton.setMaximumSize(QSize(200, 16777215))
 
-        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
-
-        self.gridLayout.addLayout(self.gridLayout_2, 1, 0, 1, 1)
-
-        self.horizontalLayout.addLayout(self.gridLayout)
-
-        self.gridLayout_3 = QGridLayout()
-        self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 1)
-
-        self.horizontalLayout.addLayout(self.gridLayout_3)
+        self.horizontalLayout.addLayout(self.mainGrid)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 33))
+        self.menubar.setGeometry(QRect(0, 0, 912, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -87,14 +108,12 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate(
-            "MainWindow", u"MainWindow", None))
-        self.labelStandard.setText(QCoreApplication.translate(
-            "MainWindow", u"VIAVI Report Generator", None))
-        self.pushButton_2.setText(QCoreApplication.translate(
-            "MainWindow", u"Convert to csv", None))
-        self.pushButton.setText(QCoreApplication.translate(
-            "MainWindow", u"Select json files", None))
-        self.label.setText(QCoreApplication.translate(
-            "MainWindow", u"EXAMPLE", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"VIAVI OLP-39 Report Generator", None))
+        self.convertButton.setText(QCoreApplication.translate("MainWindow", u"Convert to csv", None))
+        self.selectButton.setText(QCoreApplication.translate("MainWindow", u"Select json files", None))
+        self.selectedFilesLabel.setText("")
+        self.savedReportPath.setText("")
+        self.labelLogo.setText("")
+        self.labelText.setText(QCoreApplication.translate("MainWindow", u"OLP-39 Report Generator", None))
     # retranslateUi
+
