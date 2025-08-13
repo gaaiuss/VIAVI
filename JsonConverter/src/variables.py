b8b386fe40e77ee773a17b52c562ba1e6d6dcd3d
gaaiuss <caio.gui.castro@gmail.com>
2025-08-13 15:12:12 -0300

    moved to src

---
+++ JsonConverter/src/variables.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+# Directories
+ROOT_DIR = Path(__file__).parent.parent
+RESOURCE_DIR = ROOT_DIR / "resource"
+
+# Files
+JSON_OUTPUT_FILE = RESOURCE_DIR / "merged.json"
+CSV_CONFIG_FILE = RESOURCE_DIR / "config.csv"
+
+# GUI
+ICON_PATH = RESOURCE_DIR / "icon.png"
+LABEL_ICON_PATH = RESOURCE_DIR / "ui_icon.png"
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 12
+MINIMUM_WIDTH = 1000
+TEXT_MARGIN = 10
