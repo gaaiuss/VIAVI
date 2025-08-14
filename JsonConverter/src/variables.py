8e3f2aeeab6a5af5650f27722f203f93838d321a
gaaiuss <caio.gui.castro@gmail.com>
2025-08-14 15:00:25 -0300

    output dir for uneeded files

--- JsonConverter/src/variables.py
+++ JsonConverter/src/variables.py
@@ -3,9 +3,11 @@ from pathlib import Path
 # Directories
 ROOT_DIR = Path(__file__).parent.parent
 RESOURCE_DIR = ROOT_DIR / "resource"
+OUTPUT_DIR = ROOT_DIR / "output"
 
 # Files
-JSON_OUTPUT_FILE = RESOURCE_DIR / "merged.json"
+JSON_OUTPUT_FILE = OUTPUT_DIR / "merged.json"
+CSV_OUTPUT_FILE = OUTPUT_DIR / "output.csv"
 CSV_CONFIG_FILE = RESOURCE_DIR / "config.csv"
 
 # GUI
