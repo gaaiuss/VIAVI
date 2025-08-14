9b20612a2da12ec41662490397df58f26524af4f
gaaiuss <caio.gui.castro@gmail.com>
2025-08-14 15:00:58 -0300

    update ui buttons desc

--- JsonConverter/src/windowUI.py
+++ JsonConverter/src/windowUI.py
@@ -16,43 +16,57 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
-    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QStatusBar, QWidget)
+    QLayout, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(912, 703)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.mainGrid = QGridLayout()
         self.mainGrid.setObjectName(u"mainGrid")
+        self.mainGrid.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
         self.mainGrid.setContentsMargins(0, -1, -1, -1)
         self.buttonsGrid = QGridLayout()
         self.buttonsGrid.setObjectName(u"buttonsGrid")
+        self.buttonsGrid.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
         self.buttonsGrid.setHorizontalSpacing(50)
         self.buttonsGrid.setContentsMargins(100, -1, -1, -1)
-        self.convertButton = QPushButton(self.centralwidget)
-        self.convertButton.setObjectName(u"convertButton")
-        self.convertButton.setMinimumSize(QSize(0, 100))
-        self.convertButton.setMaximumSize(QSize(200, 16777215))
-
-        self.buttonsGrid.addWidget(self.convertButton, 1, 0, 1, 1)
-
-        self.selectButton = QPushButton(self.centralwidget)
-        self.selectButton.setObjectName(u"selectButton")
-        self.selectButton.setMinimumSize(QSize(0, 100))
-        self.selectButton.setMaximumSize(QSize(200, 16777215))
-
-        self.buttonsGrid.addWidget(self.selectButton, 0, 0, 1, 1)
-
         self.selectedFilesLabel = QLabel(self.centralwidget)
         self.selectedFilesLabel.setObjectName(u"selectedFilesLabel")
 
         self.buttonsGrid.addWidget(self.selectedFilesLabel, 0, 1, 1, 1)
+
+        self.selectButton = QPushButton(self.centralwidget)
+        self.selectButton.setObjectName(u"selectButton")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.selectButton.sizePolicy().hasHeightForWidth())
+        self.selectButton.setSizePolicy(sizePolicy1)
+        self.selectButton.setMinimumSize(QSize(50, 50))
+        self.selectButton.setMaximumSize(QSize(200, 16777215))
+
+        self.buttonsGrid.addWidget(self.selectButton, 0, 0, 1, 1)
+
+        self.convertButton = QPushButton(self.centralwidget)
+        self.convertButton.setObjectName(u"convertButton")
+        self.convertButton.setMinimumSize(QSize(0, 50))
+        self.convertButton.setMaximumSize(QSize(130, 16777215))
+
+        self.buttonsGrid.addWidget(self.convertButton, 1, 0, 1, 1)
 
         self.savedReportPath = QLabel(self.centralwidget)
         self.savedReportPath.setObjectName(u"savedReportPath")
@@ -65,6 +79,7 @@ class Ui_MainWindow(object):
         self.labelsLayout = QHBoxLayout()
         self.labelsLayout.setSpacing(50)
         self.labelsLayout.setObjectName(u"labelsLayout")
+        self.labelsLayout.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
         self.labelsLayout.setContentsMargins(20, -1, -1, -1)
         self.labelLogo = QLabel(self.centralwidget)
         self.labelLogo.setObjectName(u"labelLogo")
@@ -109,9 +124,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"VIAVI OLP-39 Report Generator", None))
-        self.convertButton.setText(QCoreApplication.translate("MainWindow", u"Convert to csv", None))
-        self.selectButton.setText(QCoreApplication.translate("MainWindow", u"Select json files", None))
         self.selectedFilesLabel.setText("")
+        self.selectButton.setText(QCoreApplication.translate("MainWindow", u"Select Json Files", None))
+        self.convertButton.setText(QCoreApplication.translate("MainWindow", u"Generate Report", None))
         self.savedReportPath.setText("")
         self.labelLogo.setText("")
         self.labelText.setText(QCoreApplication.translate("MainWindow", u"OLP-39 Report Generator", None))
