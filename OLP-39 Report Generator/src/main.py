225aff89ffd0ab16c42faf375e0363cd6fff1689
gaaiuss <caio.gui.castro@gmail.com>
2025-10-06 11:51:30 -0300

    save to report

--- OLP-39 Report Generator/src/main.py
+++ OLP-39 Report Generator/src/main.py
@@ -1,19 +1,30 @@
 import flet as ft  # type: ignore
 
-from converter import mergeJsonFiles
+from converter import mergeJsonFiles, openReport, saveToExcel, writeCsv
 
 
 def main(page: ft.Page):
     def select_json(e: ft.FilePickerResultEvent):
-        selected_files.value = (
-            "\n".join(map(lambda f: f.name, e.files)
-                      ) if e.files else "Cancelled!"
-        )
-        selected_files.update()
-        mergeJsonFiles(selected_files.value.split('\n'))
+        path_list = []
+        if e.files:
+            for file in e.files:  # type: ignore
+                path_list.append(file.path)
+            selected_files.value = (
+                "\n".join(map(lambda f: f.name, e.files)
+                          ) if e.files else "Cancelled!"
+            )
+            selected_files.update()
+            json_file = mergeJsonFiles(path_list)
+            writeCsv(json_file)
 
-    def generate_report(e):
-        print('Generating Report...')
+    def generate_report(e: ft.FilePickerResultEvent):
+        if e.path:
+            excel_path = saveToExcel(e.path)
+            save_dir.value = f'Report saved in: {excel_path}'
+            openReport(excel_path)
+        else:
+            save_dir.value = "Cancelled!"
+        save_dir.update()
 
     # Page Setup
     page.title = "VIAVI OLP-39 Report Generator"
@@ -30,6 +41,11 @@ def main(page: ft.Page):
     pick_files_dialog = ft.FilePicker(on_result=select_json)
     page.overlay.append(pick_files_dialog)
     selected_files = ft.Text(text_align=ft.TextAlign.CENTER)
+
+    # Generate Report
+    generate_report_dialog = ft.FilePicker(on_result=generate_report)
+    page.overlay.append(generate_report_dialog)
+    save_dir = ft.Text(text_align=ft.TextAlign.CENTER)
 
     # Text Container
     text_container = ft.Container(
@@ -52,7 +68,7 @@ def main(page: ft.Page):
         content=generate_btn_text,
         color=ft.Colors.WHITE,
         bgcolor=ft.Colors.DEEP_PURPLE_400,
-        on_click=generate_report,
+        on_click=lambda _: generate_report_dialog.get_directory_path(),
     )
 
     btn_row = ft.Row(
@@ -70,7 +86,7 @@ def main(page: ft.Page):
     )
 
     # Add Page Content
-    page.add(logo, text_container, btn_row, selected_files)
+    page.add(logo, text_container, btn_row, selected_files, save_dir)
 
 
 ft.app(main, assets_dir='assets', upload_dir='src/uploads')
